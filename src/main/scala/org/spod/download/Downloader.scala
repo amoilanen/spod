c38b040bb3b0c772bbe98f5fd348924c956e1992
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-06-20 22:01:17 +0300

    Improve resource management code for stream creation (based on the examples from the ZIO documentation)

--- src/main/scala/org/spod/download/Downloader.scala
+++ src/main/scala/org/spod/download/Downloader.scala
@@ -1,11 +1,11 @@
 package org.spod.download
 
-import java.io.{BufferedInputStream, BufferedOutputStream, File, FileOutputStream, IOException}
+import java.io.{BufferedInputStream, BufferedOutputStream, Closeable, File, FileOutputStream, IOException, InputStream, OutputStream}
 import java.net.URL
 
 import org.spod.error.SPodError
 import sttp.client3.httpclient.zio.SttpClient
-import zio.{Has, IO, ZIO, ZLayer, ZManaged}
+import zio.{Has, IO, Task, ZIO, ZLayer, ZManaged}
 import zio.console.Console
 
 object Downloader {
@@ -24,39 +24,46 @@ object Downloader {
         (console, sttpClient) =>
           new Service {
 
-            private def inputStream(link: URL): ZManaged[Any, Throwable, BufferedInputStream] =
-              ZManaged.make(ZIO.effect(new BufferedInputStream(link.openStream())))(
-                stream => ZIO.effectTotal(stream.close())
-              )
+            private def inputStreamFromLink(link: URL): Task[BufferedInputStream] =
+              ZIO.effect(new BufferedInputStream(link.openStream()))
+            private def outputStreamToFile(file: File): Task[BufferedOutputStream] =
+              ZIO.effect(new BufferedOutputStream(new FileOutputStream(file)))
+            private def close(closeable: Closeable) =
+              ZIO.effectTotal(closeable.close())
 
-            private def outputStream(file: File): ZManaged[Any, Throwable, BufferedOutputStream] =
-              ZManaged.make(ZIO.effect(new BufferedOutputStream(new FileOutputStream(file))))(
-                stream => ZIO.effectTotal(stream.close())
-              )
+            private def contentLength(link: URL): Task[Int] =
+              ZIO.effect {
+                val urlConnection = link.openConnection()
+                urlConnection.getContentLength
+              }
+
+            private def copy(in: InputStream, out: OutputStream) = {
+              val bufferSize = 1024
+              val dataBuffer = new Array[Byte](bufferSize)
+              var bytesRead = 0
+              while (bytesRead != -1) {
+                bytesRead = in.read(dataBuffer, 0, bufferSize)
+                if (bytesRead > 0) {
+                  out.write(dataBuffer, 0, bytesRead)
+                }
+              }
+              //TODO: Turn byte chunks read from the input stream into a ZStream
+              //TODO: Create ProgressBar and report progress
+              ZIO.succeed()
+            }
 
             override def download(
                 link: URL,
                 destination: File
             ): IO[SPodError, Unit] = {
-              inputStream(link).use { in: BufferedInputStream =>
-                outputStream(destination). use { out: BufferedOutputStream =>
-                  //TODO: Wrap the call to get total content length into a ZIO
-                  val urlConnection = link.openConnection()
-                  val totalContentLength = urlConnection.getContentLength
-                  val bufferSize = 1024
-                  val dataBuffer = new Array[Byte](bufferSize)
-                  var bytesRead = 0
-                  while (bytesRead != -1) {
-                    bytesRead = in.read(dataBuffer, 0, bufferSize)
-                    if (bytesRead > 0) {
-                      out.write(dataBuffer, 0, bytesRead)
-                    }
-                  }
-                  //TODO: Turn byte chunks read from the input stream into a ZStream
-                  //TODO: Create ProgressBar and report progress
-                  ZIO.succeed()
-                }
-              }.mapError(error => new SPodError("Failed to download link", Some(error)))
+              (for {
+                in <- ZManaged.make(inputStreamFromLink(link))(close)
+                out <- ZManaged.make(outputStreamToFile(destination))(close)
+                downloadableLength <- contentLength(link).toManaged_
+                _ <- copy(in, out).toManaged_
+              } yield ())
+                .mapError(error => new SPodError("Failed to download link", Some(error)))
+                .useNow
             }
           }
       }
