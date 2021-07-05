733e63876a176b68d8ef5c1059349ddea15ecd89
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-07-05 23:51:53 +0300

    Fix progress reporting for Downloader: ref was not updated

--- src/main/scala/org/spod/download/Downloader.scala
+++ src/main/scala/org/spod/download/Downloader.scala
@@ -1,13 +1,22 @@
 package org.spod.download
 
-import java.io.{BufferedInputStream, BufferedOutputStream, Closeable, File, FileOutputStream, IOException, InputStream, OutputStream}
+import java.io.{
+  BufferedInputStream,
+  BufferedOutputStream,
+  Closeable,
+  File,
+  FileOutputStream,
+  IOException,
+  InputStream,
+  OutputStream
+}
 import java.net.URL
 
 import org.spod.error.SPodError
 import org.spod.progress.ProgressBar
 import zio.blocking.Blocking
 import zio.{Has, IO, Ref, Task, ZIO, ZLayer, ZManaged}
-import zio.console.Console
+import zio.console.{Console, putStr}
 import zio.stream.{ZSink, ZStream}
 
 object Downloader {
@@ -20,50 +29,61 @@ object Downloader {
 
   val live =
     ZLayer
-      .fromFunction[Console with Blocking, Downloader.Service] {
-        env =>
-          new Service {
+      .fromFunction[Console with Blocking, Downloader.Service] { env =>
+        new Service {
 
-            private def inputStreamFromLink(link: URL): Task[BufferedInputStream] =
-              ZIO.effect(new BufferedInputStream(link.openStream()))
-            private def outputStreamToFile(file: File): Task[BufferedOutputStream] =
-              ZIO.effect(new BufferedOutputStream(new FileOutputStream(file)))
-            private def close(closeable: Closeable) =
-              ZIO.effectTotal(closeable.close())
+          private def inputStreamFromLink(
+              link: URL
+          ): Task[BufferedInputStream] =
+            ZIO.effect(new BufferedInputStream(link.openStream()))
+          private def outputStreamToFile(
+              file: File
+          ): Task[BufferedOutputStream] =
+            ZIO.effect(new BufferedOutputStream(new FileOutputStream(file)))
+          private def close(closeable: Closeable) =
+            ZIO.effectTotal(closeable.close())
 
-            private def contentLength(link: URL): Task[Int] =
-              ZIO.effect {
-                val urlConnection = link.openConnection()
-                urlConnection.getContentLength
-              }
-
-            private def copy(totalLength: Int, in: InputStream, out: OutputStream): ZIO[Console with Blocking, IOException, Long] = {
-              val progressBar = new ProgressBar(totalLength)
-              val source = ZStream.fromInputStream(in)
-              val sink = ZSink.fromOutputStream(out)
-
-              val totalProgressRef = Ref.make(0)
-              source.mapChunksM(chunk => for {
-                total <- totalProgressRef
-                total <- total.updateAndGet(x => x + chunk.length)
-                _ <- progressBar.printProgress(total)
-              } yield chunk).run(sink)
+          private def contentLength(link: URL): Task[Int] =
+            ZIO.effect {
+              val urlConnection = link.openConnection()
+              urlConnection.getContentLength
             }
 
-            override def download(
-                link: URL,
-                destination: File
-            ): IO[SPodError, Unit] = {
-              (for {
-                in <- ZManaged.make(inputStreamFromLink(link))(close)
-                out <- ZManaged.make(outputStreamToFile(destination))(close)
-                downloadableLength <- contentLength(link).toManaged_
-                _ <- copy(downloadableLength, in, out).toManaged_
-              } yield ())
-                .mapError(error => new SPodError("Failed to download link", Some(error)))
-                .useNow
-            }.provide(env)
+          private def copy(
+              totalLength: Int,
+              in: InputStream,
+              out: OutputStream
+          ): ZIO[Console with Blocking, IOException, Long] = {
+            val progressBar = new ProgressBar(totalLength)
+            val source = ZStream.fromInputStream(in)
+            val sink = ZSink.fromOutputStream(out)
+
+            Ref.make(0).flatMap({ totalProgressRef =>
+              source.mapChunksM(chunk =>
+                for {
+                  total <- totalProgressRef.updateAndGet(_ + chunk.length)
+                  _ <- progressBar.printProgress(total)
+                } yield chunk
+              ).run(sink)
+            })
           }
+
+          override def download(
+              link: URL,
+              destination: File
+          ): IO[SPodError, Unit] = {
+            (for {
+              in <- ZManaged.make(inputStreamFromLink(link))(close)
+              out <- ZManaged.make(outputStreamToFile(destination))(close)
+              downloadableLength <- contentLength(link).toManaged_
+              _ <- copy(downloadableLength, in, out).toManaged_
+            } yield ())
+              .mapError(error =>
+                new SPodError("Failed to download link", Some(error))
+              )
+              .useNow
+          }.provide(env)
+        }
       }
 
   def download(
